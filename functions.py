a8f3aa2101c72f650d2c6c64a513f21ac6b38626
Edgar Barron <edgarraul98@icloud.com>
2021-05-06 12:54:04 -0500

    best_k

--- functions.py
+++ functions.py
@@ -24,3 +24,18 @@ def plot_cf(model, x_train_data, y_train_data, x_test_data, y_test_data):
     ax1.set_title('Training Data', size=20)
     ax2.set_title('Testing Data', size=20)
     return plt.show()
+
+def find_best_k(X_train, y_train, X_test, y_test, min_k=1, max_k=25):
+    best_k = 0
+    best_score = 0.0
+    for k in range(min_k, max_k+1,2):
+        knn = KNeighborsClassifier(n_neighbors=k)
+        knn.fit(X_train, y_train)
+        preds = knn.predict(X_test)
+        f1 = f1_score(y_test, preds)
+        if f1 > best_score:
+            best_k = k
+            best_score = f1
+    
+    print('Best value for k: {}'.format(best_k))
+    print('F1 Score: {}'.format(best_score))
