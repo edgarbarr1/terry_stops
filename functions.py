eb65fa8fc52936b553dd9612caa1aa8ff66e3568
Edgar Barron <edgarraul98@icloud.com>
2021-05-07 23:22:10 -0500

    almost_done

--- functions.py
+++ functions.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression, Ridge, Lasso, LinearRegression, LassoCV
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, plot_confusion_matrix, f1_score, roc_curve, mean_squared_error, auc
+from sklearn.metrics import (confusion_matrix, plot_confusion_matrix, f1_score, 
+                             roc_curve, mean_squared_error, auc, recall_score, precision_score)
 from imblearn.over_sampling import SMOTE
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder, LabelEncoder
@@ -39,3 +40,39 @@ def find_best_k(X_train, y_train, X_test, y_test, min_k=1, max_k=25):
     
     print('Best value for k: {}'.format(best_k))
     print('F1 Score: {}'.format(best_score))
+    
+def scores(y_true_train, y_pred_train, y_true_test, y_pred_test):
+    print('F1 score for training data: {}'.format(f1_score(y_true_train, y_pred_train)))
+    print('-------------------------------------------')
+    print('F1 score for testing data: {}'.format(f1_score(y_true_test, y_pred_test)))
+    print('-------------------------------------------')
+    print('Recall score for training data: {}'.format(recall_score(y_true_train, y_pred_train)))
+    print('-------------------------------------------')
+    print('Recall score for testing data: {}'.format(recall_score(y_true_test, y_pred_test)))
+    print('-------------------------------------------')
+    print('Precision score for training data: {}'.format(precision_score(y_true_train, y_pred_train)))
+    print('-------------------------------------------')
+    print('Precision score for testing data: {}'.format(precision_score(y_true_test, y_pred_test)))
+
+    
+def roc_auc_plot(X_train, y_train, X_pred, y_pred, logreg):
+    y_score = logreg.fit(X_train, y_train).decision_function(X_pred)
+    fpr, tpr, thresholds = roc_curve(y_pred, y_score)
+    
+    sns.set_style('darkgrid', {'axes.facecolor': '0.9'})
+
+    print('AUC: {}'.format(auc(fpr, tpr)))
+    plt.figure(figsize=(10, 8))
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve')
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.yticks([i/20.0 for i in range(21)])
+    plt.xticks([i/20.0 for i in range(21)])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic (ROC) Curve')
+    plt.legend(loc='lower right')
+    plt.show()
